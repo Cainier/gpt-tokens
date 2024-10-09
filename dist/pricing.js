86cedd4750f33338fdb0901b2aca099923ac48b2
Jiawen Geng <technicalcute@gmail.com>
2024-10-09 02:00:36 +0000

    fix: gpt4o models

--- dist/pricing.js
+++ dist/pricing.js
@@ -15,16 +15,17 @@ exports.Pricing = Pricing;
  * General Model => Incremental Model
  */
 Pricing.generalModelMapping = {
+    'chatgpt-4o-latest': 'gpt-4o-2024-08-06',
+    'gpt-4o': 'gpt-4o-2024-08-06',
+    'gpt-4o-mini': 'gpt-4o-mini-2024-07-18',
+    'o1-preview': 'o1-preview-2024-09-12',
+    'o1-mini': 'o1-mini-2024-09-12',
     'gpt-3.5-turbo': 'gpt-3.5-turbo-0125',
     'gpt-3.5-turbo-16k': 'gpt-3.5-turbo-0125',
     'gpt-4': 'gpt-4-0613',
     'gpt-4-32k': 'gpt-4-32k-0613',
     'gpt-4-turbo-preview': 'gpt-4-0125-preview',
     'gpt-4-turbo': 'gpt-4-turbo-2024-04-09',
-    'gpt-4o': 'gpt-4o-2024-05-13',
-    'gpt-4o-mini': 'gpt-4o-mini-2024-07-18',
-    'o1-preview': 'o1-preview-2024-09-12',
-    'o1-mini': 'o1-mini-2024-09-12',
 };
 /**
  * Incremental Models
@@ -32,8 +33,8 @@ Pricing.generalModelMapping = {
  * Model: [Input , Output, Train (If support)] ($/1K Tokens)
  */
 Pricing.incrementalModels = {
-    'o1-preview-2024-09-12': [0.015, 0.06],
-    'o1-mini-2024-09-12': [0.003, 0.012],
+    'o1-preview-2024-09-12': [0.015, 0.06], // 2024-09-12
+    'o1-mini-2024-09-12': [0.003, 0.012], // 2024-09-12
     'gpt-4o-2024-05-13': [0.005, 0.015], // 2024-05-13
     'gpt-4o-2024-08-06': [0.0025, 0.010], // 2024-08-06
     'gpt-4o-mini-2024-07-18': [0.00015, 0.0006], // 2024-07-18
--- src/pricing.ts
+++ src/pricing.ts
@@ -41,16 +41,17 @@ export class Pricing {
     public static readonly generalModelMapping: {
         [key: string]: string
     } = {
+        'chatgpt-4o-latest'  : 'gpt-4o-2024-08-06',
+        'gpt-4o'             : 'gpt-4o-2024-08-06',
+        'gpt-4o-mini'        : 'gpt-4o-mini-2024-07-18',
+        'o1-preview'         : 'o1-preview-2024-09-12',
+        'o1-mini'            : 'o1-mini-2024-09-12',
         'gpt-3.5-turbo'      : 'gpt-3.5-turbo-0125',
         'gpt-3.5-turbo-16k'  : 'gpt-3.5-turbo-0125',
         'gpt-4'              : 'gpt-4-0613',
         'gpt-4-32k'          : 'gpt-4-32k-0613',
         'gpt-4-turbo-preview': 'gpt-4-0125-preview',
         'gpt-4-turbo'        : 'gpt-4-turbo-2024-04-09',
-        'gpt-4o'             : 'gpt-4o-2024-05-13',
-        'gpt-4o-mini'        : 'gpt-4o-mini-2024-07-18',
-        'o1-preview'         : 'o1-preview-2024-09-12',
-        'o1-mini'            : 'o1-mini-2024-09-12',
     }
 
     /**
