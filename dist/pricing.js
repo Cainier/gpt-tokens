ddb3511bc7bbc0aa025a5913da1ba7ae375598e9
Jiawen Geng <technicalcute@gmail.com>
2024-10-09 02:17:04 +0000

    fix latest pricing

--- dist/pricing.js
+++ dist/pricing.js
@@ -15,7 +15,7 @@ exports.Pricing = Pricing;
  * General Model => Incremental Model
  */
 Pricing.generalModelMapping = {
-    'chatgpt-4o-latest': 'gpt-4o-2024-08-06',
+    'chatgpt-4o-latest': 'gpt-4o-2024-05-13',
     'gpt-4o': 'gpt-4o-2024-08-06',
     'gpt-4o-mini': 'gpt-4o-mini-2024-07-18',
     'o1-preview': 'o1-preview-2024-09-12',
--- src/pricing.ts
+++ src/pricing.ts
@@ -41,7 +41,7 @@ export class Pricing {
     public static readonly generalModelMapping: {
         [key: string]: string
     } = {
-        'chatgpt-4o-latest'  : 'gpt-4o-2024-08-06',
+        'chatgpt-4o-latest'  : 'gpt-4o-2024-05-13',
         'gpt-4o'             : 'gpt-4o-2024-08-06',
         'gpt-4o-mini'        : 'gpt-4o-mini-2024-07-18',
         'o1-preview'         : 'o1-preview-2024-09-12',
